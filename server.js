9f8abead3e50aa513eafa01a6b736fcc11df8cc7
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2020-01-08 12:16:54 -0300

    update packages and add linter config

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const sgMail = require('@sendgrid/mail');
+const dotenv = require('dotenv');
 
 const app = express();
 
 // Check if is the environment is development to require dotenv
 if (app.get('env') === 'development') {
-  require('dotenv').config();
+  dotenv.config();
 }
 
 // Sendgrid API key
@@ -23,34 +24,34 @@ app.get('/', (req, res) => {
   res.json({
     Service: 'Sendgrid with Express.js',
     Status: 'Running!',
-    Repository: 'https://github.com/cobimr/sendgrid-express'
+    Repository: 'https://github.com/cobimr/sendgrid-express',
   });
 });
 
 // POST - Send Email throught SendGrid
 app.post('/send-email', async (req, res) => {
-  //Get Variables from query string in the search bar
+  // Get Variables from query string in the search bar
   const { nombre, emisor, asunto, texto } = req.body;
 
-  //Sendgrid Data Requirements
+  // Sendgrid Data Requirements
   const msg = {
     to: process.env.TARGET_EMAIL,
     from: {
       email: emisor,
-      name: nombre
+      name: nombre,
     },
     subject: asunto,
-    text: texto
+    text: texto,
   };
 
   try {
-    //Send Email
+    // Send Email
     const email = await sgMail.send(msg);
 
     // Obtener respuesta en JSON para el front-end
     res.json(email);
   } catch (err) {
-    console.log('err.message');
+    throw new Error(err.message);
   }
 });
 
